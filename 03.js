4bc3e2deee15f0aa365b78ceab83c8e677501038
Innokentii Li <lis5043@gmail.com>
2019-10-31 20:40:41 +0600

    03 added

---
+++ 03.js
@@ -0,0 +1,12 @@
+for(let i = 0; i < 4; i++){
+    var odd = '';
+    var even = '';
+    for(let i = 0; i < 4; i++){
+        odd += '██  ';
+    }
+    console.log(odd);
+    for(let i = 0; i < 4; i++){
+        even += '  ██';
+    }
+    console.log(even);
+}
