46c772d8665f80cb0a937463f8b835a4a19d54db
Innokentii Li <lis5043@gmail.com>
2019-10-31 20:28:55 +0600

    01 and 02 added

---
+++ 01.sh
@@ -0,0 +1,13 @@
+mkdir projects
+cd projects
+mkdir my_project
+cd my_project
+mkdir directory1
+cd directory1
+mkdir subdirectory1
+cd ..
+mkdir directory2
+mkdir directory3
+cd directory3
+mkdir subdirectory2
+echo "Folders have been created"
---
+++ 02.js
@@ -0,0 +1,5 @@
+const figlet = require('figlet');
+
+figlet.text(process.argv[2], function(error, success){
+    console.log(success);
+});
